468a525f6647640a2f7b85272f8ed288aa695908
Ivan Konoval <keks.ivan.k@gmail.com>
2023-03-15 20:29:57 +0100

    pytest_settings_first_tests

--- tests/authentication/test_authentication_models.py
+++ tests/authentication/test_authentication_models.py
@@ -3,10 +3,16 @@ import pytest as pytest
 from apps.authentication.models import User
 
 
-# teraz stwarza w normalnej bazie danych użytkownika i nie usuwa go, trzeba to zmienić.
 @pytest.mark.django_db
 def test_first_test():
     user = User.objects.create(
         username='test_user'
     )
-    assert User.objects.get(username='test_user').username == user.username
+    user.first_name = 'Ivan'
+    user.last_name = 'Konoval'
+    user.save()
+    user.refresh_from_db()
+
+    assert user.username == 'test_user'
+    assert user.first_name == 'Ivan'
+    assert user.last_name == 'Konoval'
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,44 @@
-import pytest
-from django.test import Client
-import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()
+from django.conf import settings
 
 
-@pytest.fixture
-def client():
-    return Client()
+def pytest_configure():
+    settings.DEBUG = False
+    settings.DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_db',
+        'USER': 'test_user',
+        'PASSWORD': 'test_password',
+        'HOST': 'localhost',
+        'PORT': '5433',
+    }
+    settings.MIDDLEWARE = [
+        'django.middleware.security.SecurityMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
+
+
+def django_db_setup():
+    from django.conf import settings
+    from django.core.management import call_command
+    from django.db import connections
+
+    settings.DEBUG = False
+
+    for alias in connections:
+        connection = connections[alias]
+        if connection.settings_dict['NAME'] == 'test_db':
+            connection.creation.create_test_db(autoclobber=True)
+
+    call_command('migrate')
+
+    yield
+
+    for alias in connections:
+        connection = connections[alias]
+        if connection.settings_dict['NAME'] == 'test_db':
+            connection.creation.destroy_test_db()
